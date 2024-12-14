6dc3815c9098f9a722801d3ebcbef8aca0a74a71
Andrei Yankavets <andreyyankovez@gmail.com>
2024-12-14 13:55:17 +0300

    [FEATURE] Implement getBook() method in ESBookServiceImpl

--- src/main/java/com/ifortex/bookservice/service/impl/ESBookServiceImpl.java
+++ src/main/java/com/ifortex/bookservice/service/impl/ESBookServiceImpl.java
@@ -1,12 +1,15 @@
 package com.ifortex.bookservice.service.impl;
 
+import com.ifortex.bookservice.BookDao.BookDao;
 import com.ifortex.bookservice.dto.SearchCriteria;
 import com.ifortex.bookservice.model.Book;
 import com.ifortex.bookservice.service.BookService;
+import com.ifortex.bookservice.util.ApplicationContextProvider;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 // Attention! It is FORBIDDEN to make any changes in this file!
 @Service
@@ -14,8 +17,25 @@ public class ESBookServiceImpl implements BookService {
 
   @Override
   public Map<String, Long> getBooks() {
-    // will be implemented shortly
-    return Map.of();
+    ApplicationContext context = ApplicationContextProvider.getApplicationContext();
+    BookDao bookDao = context.getBean(BookDao.class);
+    List<Book> books = bookDao.findAll();
+    Map<String, Long> countBooksByGenre = new HashMap<>();
+    for (Book book : books) {
+      for (var genre : book.getGenres()) {
+        countBooksByGenre.compute(genre, (k, frequency) -> frequency == null ? 1 : frequency + 1);
+      }
+    }
+    return countBooksByGenre.entrySet().stream()
+            .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+            .collect(
+                    Collectors.toMap(
+                            Map.Entry::getKey,
+                            Map.Entry::getValue,
+                            (v1, v2) -> v1 - v2,
+                            LinkedHashMap::new
+                    )
+            );
   }
 
   @Override
