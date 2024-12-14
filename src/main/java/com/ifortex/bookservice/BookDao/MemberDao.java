2ea518ea6af952707f9262067c4aec3221550e47
Andrei Yankavets <andreyyankovez@gmail.com>
2024-12-14 14:15:20 +0300

    [FEATURE] Create findMembersRegisteredIn2023WithNoBooksRead() in MemberDao

--- src/main/java/com/ifortex/bookservice/BookDao/MemberDao.java
+++ src/main/java/com/ifortex/bookservice/BookDao/MemberDao.java
@@ -5,6 +5,8 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public class MemberDao {
 
@@ -22,8 +24,19 @@ public class MemberDao {
                         ORDER BY m.membership_date DESC
                         LIMIT 1
             """;
+    private final static String FIND_MEMBERS_REGISTERED_IN_2023_WITH_NO_BOOKS_READ_SQL = """
+            SELECT m.id, m.name, m.membership_date
+                           FROM members m
+                           LEFT JOIN member_books mb ON m.id = mb.member_id
+                           WHERE date_part('year', m.membership_date) = 2023 AND mb.book_id IS NULL;
+            """;
 
     public Member findMemberByOldestRomanceBookAndRecentRegistration() {
         return (Member) entityManager.createNativeQuery(FIND_BY_OLDEST_ROMANCE_BOOK_AND_RECENT_REGISTRATION_SQL, Member.class).getSingleResult();
     }
+
+    public List<Member> findMembersRegisteredIn2023WithNoBooksRead() {
+        return entityManager.createNativeQuery(FIND_MEMBERS_REGISTERED_IN_2023_WITH_NO_BOOKS_READ_SQL, Member.class).getResultList();
+    }
+
 }
